9ff6581bfc3d08d826502c1b77cbda7b7489a845
admin <admin@123.com>
2024-12-19 16:35:15 +0530

    changes in deploy

--- Shared-library/deploy.groovy
+++ Shared-library/deploy.groovy
@@ -1,13 +1,7 @@
-def call (String environment = dev ){
-    sh 'git config --global user.name "Your Jenkins User"'
-    sh 'git config --global user.email "your-email@example.com"'
-    
-    // Perform the git tag operation or any other Git-related task
-    sh 'git tag -a -f -m "Jenkins Build" my-tag'
-    if (environment == dev){
+def call(String environment = 'dev') {
+    if (environment == 'dev') {
         echo "Deploy in dev stage"
-    }
-    else (environment == UAT ){
+    } else if (environment == 'UAT') {
         echo "Deploy in UAT stage"
     }
 }
