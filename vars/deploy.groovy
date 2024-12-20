bbc215204f4ce6c06a99a2e7dca583ce68d4222d
thanikaichandran.cj <thanikaichandran.cj@sciflare.com>
2024-12-20 09:26:47 +0530

    small changes in file

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,17 +1,7 @@
-// In the vars/deploy.groovy file of your shared library
-def call(String environment) {
-    echo "Deploying to ${environment} environment."
-    
-    // Add your actual deployment logic based on the environment passed (e.g., UAT, PROD)
-    if (environment == 'UAT') {
-        echo "Deploying to UAT environment..."
-        // Add UAT specific deployment commands here
-        // For example: sh 'deploy-to-uat.sh'
-    } else if (environment == 'PROD') {
-        echo "Deploying to PROD environment..."
-        // Add PROD specific deployment commands here
-        // For example: sh 'deploy-to-prod.sh'
-    } else {
-        echo "Unknown environment: ${environment}"
+def call(String name = "Alice") {
+    script {
+        sh """
+            echo Hi ${name}
+        """
     }
 }
