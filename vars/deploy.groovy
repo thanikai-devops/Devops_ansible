2e14277412fac0e6c73be31066644cc66d2e8634
thanikaichandran.cj <thanikaichandran.cj@sciflare.com>
2024-12-20 09:53:07 +0530

    modify jenkins pipeline

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,10 +1,13 @@
-def call(String environment = "dev") {
+def call(String environment = "prod") {
     script {
        if (environment == "dev"){
         echo "Deploying to Development (dev) environment."
        }
        else (environment == "UAT"){
         echo "Deploying to Development (UAT) environment."
+       }
+       else (environment == "Prod"){
+        echo "Deploying to prod environment"
        }
     }
 }
