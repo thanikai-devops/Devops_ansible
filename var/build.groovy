768313807c63618aa12f68af58489e5343893b16
thanikaichandran.cj <thanikaichandran.cj@sciflare.com>
2024-12-31 12:37:50 +0530

    changes in build stage

--- var/build.groovy
+++ var/build.groovy
@@ -1,3 +1,6 @@
-def call(){
-    echo "Build the jenkins JOb Success"
+// vars/build.groovy
+
+def call() {
+    echo "Running the build process..."
+    // Add your build steps here, e.g., invoking shell commands, calling other scripts, etc.
 }
