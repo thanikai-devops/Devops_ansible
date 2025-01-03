ba1bceff4a7519d10dbd5a1a49c7fab7d6c5a257
thanikaichandran.cj <thanikaichandran.cj@sciflare.com>
2025-01-03 11:57:06 +0530

    Using parameters

--- vars/checkoutgoo.groovy
+++ vars/checkoutgoo.groovy
@@ -1,4 +1,4 @@
-def call(String branch= "seran_dev",String repourl='https://SciflareIT@bitbucket.org/dpl11-backend/goo-plus.git'
+def call(String branch= "",String repourl=""
 ,String credentialsId = 'BitbucketRepo') {
     // Fetch the Git configuration based on the project
     checkout([
