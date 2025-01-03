fba1b744477f4b828c7e9f43069833266f797bd9
thanikaichandran.cj <thanikaichandran.cj@sciflare.com>
2025-01-03 11:25:57 +0530

    checkout changes modifyed

--- vars/checkoutRepo.groovy
+++ vars/checkoutRepo.groovy
@@ -1,10 +1,10 @@
-def call() {
+def call(String branch= "seran_dev",String repourl='https://SciflareIT@bitbucket.org/dpl11-backend/goo-plus.git') {
     // Fetch the Git configuration based on the project
     checkout([
         $class: "GitSCM",
-        branches: [[name: "*/seran-dev"]],
+        branches: [[name: "*/${branch}"]],
         userRemoteConfigs: [[
-            url: 'https://SciflareIT@bitbucket.org/dpl11-backend/goo-plus.git',
+            url: repourl,
             credentialsId: credentials("BitbucketRepo")
         ]]
     ])
