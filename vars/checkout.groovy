5c0ed9f1d851586e650c8efe483931f6cbf90632
thanikaichandran.cj <thanikaichandran.cj@sciflare.com>
2025-01-03 10:38:29 +0530

    checkout changes modifyed

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,9 +1,6 @@
-def call(String project,def credentials) {
-    // Get the Git configuration for the project
-   def config = gitconfig(project,credentails)
-
-   echo "Using Bitbucket credentails with username: ${config.GITHUB_CREDS}"
-   echo "Checking out repository: ${config.git_url} on branch ${config.git_branch}"
+def call(String project) {
+    // Fetch the Git configuration based on the project
+    def config = gitconfig(project)
+    url: config.git_url
+    branch: config.git_branch
 }
-
-
