d81e63b0058ad5cb264b3cf817fddcb1f2a93d28
Thanikai <128052851+ThanikaiCloudevops@users.noreply.github.com>
2025-01-03 12:07:29 +0530

    Update checkoutgoo.groovy

--- vars/checkoutgoo.groovy
+++ vars/checkoutgoo.groovy
@@ -10,3 +10,21 @@ def call(String branch= "seran_dev",String repourl='https://SciflareIT@bitbucket
         ]]
     ])
 }
+IF USING PARAMETERS
+// vars/checkoutRepo.groovy
+// def call(Map params) {
+//     def branch = params.get('branch', 'dev_dafin')  // Default branch if not provided
+//     def repoUrl = params.get('repoUrl', 'https://SciflareIT@bitbucket.org/dpl11-backend/goo-plus.git')  // Default repo URL
+//     def credentialsId = params.get('credentialsId', 'BitbucketRepo')  // Default credentials ID
+
+//     script {
+//         checkout([
+//             $class: 'GitSCM',
+//             branches: [[name: "*/${branch}"]],
+//             userRemoteConfigs: [[
+//                 url: repoUrl,
+//                 credentialsId: credentialsId
+//             ]]
+//         ])
+//     }
+// }
