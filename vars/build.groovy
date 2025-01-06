4bc5dc02b474b8cdb5c43bfce09b34b01145833a
thanikaichandran.cj <thanikaichandran.cj@sciflare.com>
2025-01-06 12:44:36 +0530

    Buildstage working

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,11 +1,5 @@
 
 def call() {
     // Add your build steps here, e.g., invoking shell commands, calling other scripts, etc.
-    stage("Checking the version and install the dep"){
-        steps{
-            script{
-                sh "node -v"
-            }
-        }
-    }
+    sh "node -v"
 }   
