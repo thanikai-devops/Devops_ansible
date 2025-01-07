3fa38b4f3729944c031e5e1ef1ab66eddb38ff61
thanikaichandran.cj <thanikaichandran.cj@sciflare.com>
2025-01-07 13:17:52 +0530

    edit jenkisfile and docker

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,13 +1,12 @@
 def call(String gitbranch="seran_dev" , String repourl= "https://SciflareIT@bitbucket.org/dpl11-backend/goo-plus.git", String credentialsId = "BitbucketRepo" ) {
     // Fetch the Git configuration based on the project
-    def scmConfig= [
+    checkout([
         $class: "GitSCM",
         branches: [[name: "*/${gitbranch}"]],
         userRemoteConfigs: [[
             url: repourl,
             credentialsId: credentialsId
         ]]
-    ]
-    checkout scm: scmConfig
+    ])
 }
 
