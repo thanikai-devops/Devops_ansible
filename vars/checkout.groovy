98fae777a65d72c74a4633cf2e54d45a5f90e680
thanikaichandran.cj <thanikaichandran.cj@sciflare.com>
2025-01-07 13:46:27 +0530

    edit jenkisfile and docker

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,4 +1,5 @@
-def call(String gitbranch="seran_dev" , String repourl= "https://SciflareIT@bitbucket.org/dpl11-backend/goo-plus.git", String credentialsId = "BitbucketRepo" ) {
+def call(String gitbranch="seran_dev" , String repourl= "https://SciflareIT@bitbucket.org/dpl11-backend/goo-plus.git",
+ String credentialsId = "BitbucketRepo" ) {
     // Fetch the Git configuration based on the project
     checkout([
         $class: "GitSCM",
