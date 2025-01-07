36b668be837cfa609f4f405b89e41dc2162d00c1
thanikaichandran.cj <thanikaichandran.cj@sciflare.com>
2025-01-07 13:09:07 +0530

    edit jenkisfile and docker

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,12 +1,13 @@
 def call(String gitbranch, String repourl, String credentialsId = "BitbucketRepo" ) {
     // Fetch the Git configuration based on the project
-    checkout([
+    def scmConfig= [
         $class: "GitSCM",
         branches: [[name: "*/${gitbranch}"]],
         userRemoteConfigs: [[
             url: repourl,
             credentialsId: credentialsId
         ]]
-    ])
+    ]
+    checkout(scmConfig)
 }
 
