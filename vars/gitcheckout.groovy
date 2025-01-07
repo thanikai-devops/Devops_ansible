a84992a360cf0522839726eebd96c500c894acce
thanikaichandran.cj <thanikaichandran.cj@sciflare.com>
2025-01-07 16:14:54 +0530

    env for checkout-2

--- vars/gitcheckout.groovy
+++ vars/gitcheckout.groovy
@@ -1,7 +1,7 @@
 def call(String gitbranch , String repourl,String credentialsId = "BitbucketRepo" ) {
     // Fetch the Git configuration based on the project
-    gitbranch = gitbranch ?: System.getenv('GIT_BRANCH') ?: error("Branch must be provided")
-    repourl = repourl ?: System.getenv('REPO_URL') ?: error("Repository URL must be provided")
+    def gitbranch = System.getenv('GIT_BRANCH') ?: error('Branch Name must be provided')  // Default to 'main' if GIT_BRANCH is not set
+    def repourl = System.getenv('REPO_URL') ?: error('Repository URL must be provided')
     checkout([
         $class: "GitSCM",
         branches: [[name: "*/${gitbranch}"]],
