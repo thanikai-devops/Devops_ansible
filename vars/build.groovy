c05e51596be5640674e19cd380d3f15958ca72fa
thanikaichandran.cj <thanikaichandran.cj@sciflare.com>
2025-01-08 16:12:11 +0530

    pushing checkout file and Docker file

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,11 +1,12 @@
 // vars/build.groovy
 // Environment variables
-def call(String imageName,String imageTag,String dockerfilepath) {
+def call(String imageName='sample',String imageTag='latest',String dockerfilepath="./Dockerfile") {
     // Step 1: Check Docker version
     echo "Checking Docker version..."
     sh "docker --version"
     // Step 2: Build Docker image
     echo "Pulling Docker image ${imageName}:${imageTag}..."
+
     sh """
         docker build -t  ${imageName}:${imageTag} -f ${dockerfilepath} .
     """ 
