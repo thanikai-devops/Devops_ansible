db9721d28636a046b994cae468c02554255010e6
thanikaichandran.cj <thanikaichandran.cj@sciflare.com>
2025-01-08 13:16:41 +0530

    Modify checkout

--- vars/checkoutCode.groovy
+++ vars/checkoutCode.groovy
@@ -1,5 +1,9 @@
-def call(String gitbranch="" , String repourl="",String credentialsId = "BitbucketRepo" ) {
+def call() {
     // Fetch the Git configuration based on the project
+    def gitbranch = env.GIT_BRANCH ?: ""
+    def repourl = env.REPO_URL ?: ""
+    def credentialsId = env.credentialsId ?: "BitbucketRepo"
+    echo "Checking out from repo: ${repourl}, branch: ${gitbranch} with credentials: ${credentialsId}"
     checkout([
         $class: "GitSCM",
         branches: [[name: "*/${gitbranch}"]],
