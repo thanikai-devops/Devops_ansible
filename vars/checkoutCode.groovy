96223910d3707bf982f37cc3d3cf14c757dfb094
thanikaichandran.cj <thanikaichandran.cj@sciflare.com>
2025-01-08 13:22:10 +0530

    checkout

--- vars/checkoutCode.groovy
+++ vars/checkoutCode.groovy
@@ -1,8 +1,8 @@
-def call() {
+def call(String repoUrl="https://SciflareIT@bitbucket.org/dpl11-backend/goo-plus.git",String branch= "seran_dev",String credentialsId="BitbucketRepo") {
     // Fetch the Git configuration based on the project
-    def gitbranch = env.GIT_BRANCH ?: ""
-    def repourl = env.REPO_URL ?: ""
-    def credentialsId = env.credentialsId ?: "BitbucketRepo"
+    // def gitbranch = env.GIT_BRANCH ?: ""
+    // def repourl = env.REPO_URL ?: ""
+    // def credentialsId = env.credentialsId ?: "BitbucketRepo"
     echo "Checking out from repo: ${repourl}, branch: ${gitbranch} with credentials: ${credentialsId}"
     checkout([
         $class: "GitSCM",
