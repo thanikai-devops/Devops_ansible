26cdf36c294f6644b87def38e296c917cfe1d4d7
thanikaichandran.cj <thanikaichandran.cj@sciflare.com>
2025-01-08 16:39:16 +0530

    pushing checkout file and Docker file

--- vars/build.groovy
+++ vars/build.groovy
@@ -7,12 +7,15 @@ def call(String imageName='sample',String imageTag='latest',String dockerfilepat
     // Step 2: Build Docker image
     echo "Pulling Docker image ${imageName}:${imageTag}..."
 
+    // sh """
+    //     docker build -t  ${imageName}:${imageTag} ${dockerfilepath}
+    // """ 
     sh """
-        docker build -t  ${imageName}:${imageTag} ${dockerfilepath}
-    """ 
-    sh """
-        docker run --rm ${imageName}:${imageTag}
+        docker build . -f /resources/gooplus/Dockerfile
     """
+    // sh """
+    //     docker run --rm ${imageName}:${imageTag}
+    // """
     // echo "Building Docker image ${imageName}:${imageTag} from Dockerfile at ${dockerfilePath}..."
     // sh """
     //     docker build -t ${imageName}:${imageTag} -f ${dockerfilePath} .
