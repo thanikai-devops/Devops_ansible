c43e8004a7ca19f2c3b353e124b74af5cd40d219
thanikaichandran.cj <thanikaichandran.cj@sciflare.com>
2025-01-09 11:50:15 +0530

    Build stage with dockerfile

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -1,14 +1,18 @@
 // vars/build.groovy
 // Environment variables
-def call(String imageName,String imageTag,String dockerfilepath) {
+def call(String imageName,String imageTag,String dockerfilepath, String dockerimagename="gooplusapi") {
     // Step 1: Check Docker version
     echo "Checking Docker version..."
     sh "docker --version"
+    def tag = env.BUILD_ID
     // Step 2: Build Docker image
     echo "Pulling Docker image ${imageName}:${imageTag}..."
     sh """
-        docker build -t  ${imageName}/${imageTag} .
+        docker build -t  ${imageName}:${imageTag} -f ${dockerfilepath}
     """ 
+    sh """
+        docker tag ${imageName}:${imageTag} ${dockerimagename}:${tag}
+    """
 }
 
 
