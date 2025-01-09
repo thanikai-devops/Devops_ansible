9acc612eb4af6aec86840b0d4706811b60fdd676
thanikaichandran.cj <thanikaichandran.cj@sciflare.com>
2025-01-09 11:30:46 +0530

    dockerfile modifty

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -12,31 +12,3 @@ def call(String imageName,String imageTag,String dockerfilepath) {
 }
 
 
-
-
-
-
-// def call(String appName , String appDir ) {
-//     script {
-//         // Step 1: Prepare the build context directory for Docker
-//         def buildContext = "./docker-context-${appName}"
-        
-//         // Create the build context directory
-//         sh "mkdir -p ${buildContext}"
-
-//         // Step 2: Copy the application code to the build context directory
-//         sh "cp -r ${appDir}/* ${buildContext}/"
-
-//         // Step 3: Copy the Dockerfile template to the build context
-//         sh "cp resources/Dockerfile.template ${buildContext}/Dockerfile"
-
-//         // Step 4: Build the Docker image using the prepared build context
-//         def image = docker.build("${appName}-image", buildContext)
-
-//         // Optional Step: Run the Docker container (for testing)
-//         docker.image(image).run('-d -p 3000:3000')
-
-//         // Clean up
-//         sh "rm -rf ${buildContext}" // Clean up build context after the build
-//     }
-// }
