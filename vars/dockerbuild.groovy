ed9e4fa4278e290969729e8c61eb29cad6ff1ea2
thanikaichandran.cj <thanikaichandran.cj@sciflare.com>
2025-01-10 12:15:40 +0530

    ansible folder

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -6,9 +6,9 @@ String ansiblepath) {
     echo "Checking Docker version..."
     sh "docker --version"
     
-    def ansiblePlaybookPath = "/ansible"
+    def ansiblePlaybookPath = "/ansible/ecr_create.yml"
     echo "showing ${ansiblePlaybookPath}"
-    def inventoryPath = "./ansible/inventory.ini"
+    def inventoryPath = "/ansible/inventory.ini"
     def tag = env.BUILD_ID
     // Step 2: Build Docker image
     echo "Pulling Docker image ${imageName}:${imageTag}..."
@@ -18,7 +18,7 @@ String ansiblepath) {
     // Creating Aws Ecr repo using ansible files
     echo "Creating Aws ecr repo for ${aws_repoName}"
     sh """
-        ansible-playbook  -i ${inventoryPath} ${ansiblePlaybookPath}  --extra-vars "ecr_repo_name=${aws_repoName}"
+        ansible-playbook  -i ${inventoryPath}  ${ansiblePlaybookPath} --extra-vars "ecr_repo_name=${aws_repoName}"
     """
 }
 
