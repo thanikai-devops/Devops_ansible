bf84f02a98dbe2486eb76e5a939ca7b5371d9db3
thanikaicloudevops@gmail.com <thanikaicloudevops@gmail.com>
2025-01-17 10:05:59 +0530

    checking ecr repo working or not

--- vars/dockerpush.groovy
+++ vars/dockerpush.groovy
@@ -3,8 +3,7 @@ String dockerimagename="829173323501.dkr.ecr.ap-south-1.amazonaws.com/gooplus:la
     // docker tagged and push into aws ecr repo
     // aws login into the aws ECR
     sh """
-    aws ecr get-login-password --region ap-south-1 | docker 
-    login --username AWS --password-stdin 829173323501.dkr.ecr.ap-south-1.amazonaws.com
+    aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 829173323501.dkr.ecr.ap-south-1.amazonaws.com
     """
     def tag = env.BUILD_ID
     // Tagging the docker Images 
