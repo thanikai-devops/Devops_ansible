bf5c558cbd5ebe57eab2be2fe85406278dff1da4
thanikaichandran.cj <thanikaichandran.cj@sciflare.com>
2025-01-21 17:37:56 +0530

    written for deploy the docker image into application server

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,4 +1,6 @@
-def deploydocker(String ansible ){
+def deploydocker(){
     // Application deploy into server using ansible
-     
+     dir("/opt/workspace/GooplusBuildDeploy/playbook"){
+        sh "ansible-playbook -i inventory goodeploy.yml"
+     }
 }
