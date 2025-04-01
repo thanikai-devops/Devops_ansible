11ef3ff4e4e4969efa801c93c6bfe9a57517f7ab
thanikaichandran.cj <thanikaichandran.cj@sciflare.com>
2025-04-01 15:33:56 +0530

    modify dockerfile

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,20 +1,20 @@
-def ecrLoginBuildAndPush(String awsRegion="ap-south-1", String ecrRepos, String buildNumber,String dockerfilepath) {
-    echo "Logging into AWS ECR Repository: ${ecrRepos} in region: ${awsRegion}"
+def ecrLoginBuildAndPush(String awsRegion="ap-south-1", String ecrRepo, String buildNumber) {
+    echo "Logging into AWS ECR Repository: ${ecrRepo} in region: ${awsRegion}"
     
     // Step 1: Login to AWS ECR using AWS CLI
     sh """
-        aws ecr get-login-password --region ${awsRegion} | docker login --username AWS --password-stdin ${ecrRepos}
+        aws ecr get-login-password --region ${awsRegion} | docker login --username AWS --password-stdin ${ecrRepo}
     """
     
     echo "Docker Clean up"
     sh "docker system prune -f -a"
 
     // Step 2: Build Docker Image
-    echo "Building Docker image: ${ecrRepos}:${buildNumber}"
-    sh "docker build -t ${ecrRepos}:${buildNumber} ."
+    echo "Building Docker image: ${ecrRepo}:${buildNumber}"
+    sh "docker build -t ${ecrRepo}:${buildNumber} ."
 
     // Step 3: Push Docker Image to AWS ECR
-    echo "Pushing Docker image to AWS ECR: ${ecrRepos}:${buildNumber}"
-    sh "docker push ${ecrRepos}:${buildNumber}"
+    echo "Pushing Docker image to AWS ECR: ${ecrRepo}:${buildNumber}"
+    sh "docker push ${ecrRepo}:${buildNumber}"
 
 }
